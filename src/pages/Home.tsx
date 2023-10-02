ffb9729474da63bdcc7cf9409ea244a3edab357a
laaledesiempre <128645176+laaledesiempre@users.noreply.github.com>
2023-10-02 13:04:29 +0000

    Added basic syntax, folders and boiler plate (non tested)

---
+++ src/pages/Home.tsx
@@ -0,0 +1,15 @@
+import "./home.css"
+export const Home= () => (
+	<section class="home-wrapper">
+        <article>
+            <img href="#"/>
+            <h3>name</h3>
+            <p>random phrase</p>
+        </article>
+        <article>
+            <button>Feed</button>
+            <button>Pet</button>
+            <button>Play</button>
+        </article>
+    </section>
+        )
